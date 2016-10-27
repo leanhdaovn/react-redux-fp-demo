cb5e73fff386947f9fc96a30f139a5aac9c15de7
Dao Le <leanhdaovn@gmail.com>
2016-10-27 12:09:02 +0700

    use lodash/fp

--- src/components/SuburbSelect/state.js
+++ src/components/SuburbSelect/state.js
@@ -1,27 +1,15 @@
 import { createAction, handleAction } from 'redux-actions';
 import reduceReducers from 'reduce-reducers';
+import { get } from 'lodash/fp'
 
-export const suburbSelector = (state) => state.suburb ? state.suburb.value : null;
+export const suburbSelector = get('suburb.value');
 
 const CHANGE_SUBURB = 'SuburbSelect/CHANGE_SUBURB';
-// const CLEAR_SEARCH_STRING = 'SuburbSelect/CLEAR_SUBURB'
 
 export const changeSuburb = createAction(CHANGE_SUBURB);
-// export const clearSuburbSearchString = createAction(CLEAR_SEARCH_STRING);
 
 export const changeSuburbReducer = handleAction(changeSuburb, (state, action) => {
-  console.log(state, action);
   return action && action.payload ? action.payload : state;
 }, null);
-
-// const clearSuburbSearchStringReducer = handleAction(clearSuburbSearchString, (state = '', action) => {
-//   console.log(state);
-//   return state;
-// }, '');
-
-// export const reducer = reduceReducers(
-//   changeSuburbSearchStringReducer,
-//   clearSuburbSearchStringReducer
-// );
 
 export default changeSuburbReducer;
--- src/components/VisibleUserList/state.js
+++ src/components/VisibleUserList/state.js
@@ -1,9 +1,31 @@
+import { flow, get, toLower, invokeArgs, gt, getOr, filter, constant, identity } from 'lodash/fp';
+
+const branch = (test, left, right) => (...params) => test(...params) ? left(...params) : right(...params);
+
+const printLog = args => {
+  console.log(args);
+  return args;
+};
+
+const getLowerCaseName = flow(get('name'), toLower);
+const include = str => flow(invokeArgs('indexOf', [str]), x => x > -1);
+
+const hasString = string => filter(flow(
+  getLowerCaseName,
+  include(string)
+));
+
+const getUsers = getOr([], 'users');
+const getSearchString = getOr('', 'userSearchString');
+
 export const filteredUsersSelector = (state = []) => {
-  if (state && state.users){
-    return state.users
-            .filter(u => u.name.toLowerCase()
-            .indexOf(state.userSearchString) > -1);
-  } else {
-    return [];
-  }
+  const searchString = getSearchString(state);
+  return flow(
+    getUsers,
+    branch(
+      constant(searchString),
+      hasString(searchString),
+      identity
+    )
+  )(state);
 };
